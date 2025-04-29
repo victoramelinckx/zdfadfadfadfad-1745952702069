10bf703d47403d1b40d5516a29de747a047da9ec
Victor Amelinckx <68256527+victoramelinckx@users.noreply.github.com>
2025-04-29 14:52:58 -0400

    Add src/components/X.tsx

---
+++ src/components/X.tsx
@@ -0,0 +1,11 @@
+import { CgClose } from "react-icons/cg";
+
+const X = () => {
+  return (
+    <div className="flex flex-col py-4 px-4 rounded-[10px]  bg-[#d2d7ff19]  gap-[5px] relative ">
+      <CgClose className="text-white w-5 h-full" />
+    </div>
+  );
+};
+
+export default X;
