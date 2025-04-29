c899da51866dcab6ca84b31b01ed192bc2e25f3e
Victor Amelinckx <68256527+victoramelinckx@users.noreply.github.com>
2025-04-29 14:53:07 -0400

    Add src/components/Bars.tsx

---
+++ src/components/Bars.tsx
@@ -0,0 +1,11 @@
+import { CgMenuRight } from "react-icons/cg";
+
+const Bars = () => {
+  return (
+    <div className="flex flex-col py-3 px-4 rounded-[10px]  bg-[#d2d7ff19]  gap-[5px] ">
+      <CgMenuRight className="text-white w-6 h-full" />
+    </div>
+  );
+};
+
+export default Bars;
